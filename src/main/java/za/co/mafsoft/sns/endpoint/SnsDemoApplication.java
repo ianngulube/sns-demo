fedb2a1918f6be476fecf921ea3b1c7ea773aedc
Ian <ngulubeian94@gmail.com>
2022-12-06 18:38:47 +0200

    Test

---
+++ src/main/java/za/co/mafsoft/sns/endpoint/SnsDemoApplication.java
@@ -0,0 +1,13 @@
+package za.co.mafsoft.sns.endpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnsDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/za/co/mafsoft/sns/endpoint/SnsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.mafsoft.sns.endpoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
